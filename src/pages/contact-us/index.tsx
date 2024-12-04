a313515c10c2162851ad2ccc9d1a701fab4bfe2a
Adithya Nuz Pratama Putra <adithyanuzpratamaputra@gmail.com>
2024-12-04 09:42:14 +0700

    fixx

--- src/pages/contact-us/index.tsx
+++ src/pages/contact-us/index.tsx
@@ -1,9 +1,8 @@
 import { useState } from "react";
 import { Row, Col, Form, Input, Button, Checkbox, Typography } from "antd";
 import Head from "next/head";
-import useWindowSize from "@utils/helpers/ReactHelper";
-import Header from "@components/Global/Header/Header";
 import Image from "next/image";
+import useWindowSize from "@utils/helpers/ReactHelper";
 import { PushNavigateTo } from "@utils/helpers/Route";
 import { buttonStyle } from "@components/home/carousel/styles";
 
@@ -11,38 +10,32 @@ const { Title, Text } = Typography;
 
 const ContactPage = () => {
     const [form] = Form.useForm();
-
     const [loading, setLoading] = useState(false);
-
     const { isMobile } = useWindowSize();
-    const [drawerVisible, setDrawerVisible] = useState(false);
-
-    const toggleDrawer = () => {
-        setDrawerVisible(!drawerVisible);
-    };
 
     const handleOk = () => {
-        form.validateFields().then(values => {
-            const { fullName, phoneNumber, company, email, message, natureOfRequest } = values;
+        form.validateFields()
+            .then(values => {
+                const { fullName, phone, company, email, message } = values;
 
-            const subject = `Technical Data Sheet Request from ${fullName}`;
-            const body = `
-                Full Name: ${fullName}
-                Phone Number: ${phoneNumber}
-                Company: ${company}
-                E-Mail: ${email}
-                Message: ${message}
-            `;
+                const subject = `Technical Data Sheet Request from ${fullName}`;
+                const body = `
+                    Full Name: ${fullName}
+                    Phone Number: ${phone}
+                    Company: ${company}
+                    E-Mail: ${email}
+                    Message: ${message}
+                `;
 
-            const recipientEmail = "info@ptayesproinovasi.com";
+                const recipientEmail = "info@ptayesproinovasi.com";
+                const mailLink = `https://mail.google.com/mail/?view=cm&fs=1&to=${encodeURIComponent(recipientEmail)}&su=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+                window.open(mailLink, "_blank");
 
-            const gmailLink = `https://mail.google.com/mail/?view=cm&fs=1&to=${encodeURIComponent(recipientEmail)}&su=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
-            window.open(gmailLink, "_blank");
-
-            form.resetFields();
-        }).catch(info => {
-            console.error("Validate Failed:", info);
-        });
+                form.resetFields();
+            })
+            .catch(info => {
+                console.error("Validation Failed:", info);
+            });
     };
 
     const buttonResponsiveStyle = {
@@ -56,43 +49,35 @@ const ContactPage = () => {
             <Head>
                 <title>Contact Us</title>
             </Head>
-            {/* <Header toggleDrawer={toggleDrawer} /> */}
 
-            <div style={{ padding: isMobile ? "20px" : "30px", maxWidth: "1600px", margin: "0 auto" }}>
-                <Row justify={"space-around"} align={"middle"}>
-                    <Col span={20}>
-                        <Title
-                            level={2}
-                            className="title-font"
-                            style={{ fontSize: "48px" }}
-                        >
+            <div
+                className={"container"}
+                style={{ padding: isMobile ? "20px" : "30px", maxWidth: "1600px", margin: "0 auto" }}
+            >
+                <Row justify="space-between" align="top" className={"headerRow"}>
+                    <Col xs={20}>
+                        <Title level={2} className={"title"} style={{ marginTop: 100 }}>
                             Contact Us
                         </Title>
-                        <Text type="secondary">
+                        <Text className={"subtitle"}>
                             Be free to get in touch and we’d love to help
                         </Text>
                     </Col>
-                    <Col xs={4} sm={4} md={4} lg={4} >
-                        <Row justify={"end"}>
-                            <Image
-                                src="/logo_primary.png"
-                                alt="Company Logo"
-                                width={130}
-                                height={90}
-                                onClick={() => {
-                                    PushNavigateTo("/")
-                                }}
-                            />
-                        </Row>
+                    <Col xs={4} className={"logoCol"}>
+                        <Image
+                            src="/logo_primary.png"
+                            alt="Company Logo"
+                            width={130}
+                            height={90}
+                            className={"logo"}
+                            onClick={() => PushNavigateTo("/")}
+                        />
                     </Col>
                 </Row>
-                <Row gutter={16} style={{ marginTop: "30px", height: "990px" }}>
 
+                <Row gutter={16} style={{ marginTop: "30px" }} className={"mainContent"}>
                     <Col xs={24} md={14}>
-                        <Form
-                            layout="vertical"
-                            initialValues={{ remember: true }}
-                        >
+                        <Form layout="vertical" form={form} className={"form"}>
                             <Row gutter={16}>
                                 <Col xs={24} md={12}>
                                     <Form.Item
@@ -130,7 +115,7 @@ const ContactPage = () => {
                                         name="email"
                                         rules={[
                                             { required: true, message: "Please input your email!" },
-                                            { type: "email", message: "Please input a valid email!" }
+                                            { type: "email", message: "Please input a valid email!" },
                                         ]}
                                     >
                                         <Input placeholder="somebody@mail.com" />
@@ -158,10 +143,11 @@ const ContactPage = () => {
                             <Form.Item>
                                 <Button
                                     type="primary"
-                                    onClick={() => handleOk() }
+                                    onClick={handleOk}
                                     loading={loading}
                                     style={buttonResponsiveStyle}
                                     block
+                                    className={"submitButton"}
                                 >
                                     Send the message
                                 </Button>
@@ -170,17 +156,19 @@ const ContactPage = () => {
                     </Col>
 
                     <Col xs={0} md={10}>
-                        <Row justify={"end"}>
-                            <Image
-                                src="/Rectangle_8.png"
-                                alt="Contact Us"
-                                layout="fill"
-                                width={"100%"}
-                                style={{ maxWidth: "100%"}}
-                            />
-                        </Row>
                     </Col>
                 </Row>
+                {!isMobile && (
+                    <div className={"fixedImage"}>
+                        <Image
+                            src="/Rectangle_8.png"
+                            alt="Contact Us"
+                            layout="responsive"
+                            height={600}
+                            width={500}
+                        />
+                    </div>
+                )}
             </div>
         </>
     );
